430c64abd1c81fe3b43e0e9641eeece5c479167e
zahrank21 <zahrank@gmail.com>
2018-01-16 13:00:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ end
 
 def merge_data(keys, data)
   keys.values.each do |name|
-    if data.keys.inlcude?(name)
+    if data.keys.include?(name)
       data[name][:first_name] = name
     end
   end
