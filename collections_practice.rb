6e51c54d32042dcb5ee29ece9a7170f0d31e57c2
zahrank21 <zahrank@gmail.com>
2018-01-16 12:29:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all?{|el| el[0].downcase == "r"}
+end
+
+def contain_a(arr)
+  arr.select{|el| el.downcase.include?("a")}
+end
+
+def first_wa(arr)
+  arr.select{|el| el[0..1].downcase == "wa"}.first
+end
+
+def remove_non_strings(arr)
+  arr.select{|el| el.class == String}
+end
+
+def count_elements(arr)
+  counter_hash = Hash.new(0)
+  arr.each
+end
